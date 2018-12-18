722d6c66b7e545cfffa42cfee7b3d7bb52e30911
Claire Muller <clairermuller@gmail.com>
2018-12-18 05:50:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,17 +14,15 @@ function divide(a, b) {
   return a / b
 }
 
-function inc(n) {
-  return 
+function inc(a) {
+  return a + 1
+}
+
+function dec(a) {
+  return a - 1
 }
 
 /*
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
 
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
 
 */
